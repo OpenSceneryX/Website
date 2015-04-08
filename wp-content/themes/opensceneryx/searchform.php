39d3c3bb48ac88cf84d02d554306899045aa0711
aussig <austin@opensceneryx.com>
2015-04-08 16:07:21 +0100

    Tweak to search form styling.

--- wp-content/themes/opensceneryx/searchform.php
+++ wp-content/themes/opensceneryx/searchform.php
@@ -1,10 +1,9 @@
 <form action="http://www.google.co.uk" id="searchform" target="_blank">
-  <div>
-    <input type="hidden" name="cx" value="partner-pub-5631233433203577:vypgar-6zdh" />
-    <input type="hidden" name="ie" value="UTF-8" />
-    <input type="text" name="q" size="31" />
-    <input type="submit" name="sa" value="Search" class="submit" id="searchsubmit" />
-  </div>
+  <input type="hidden" name="cx" value="partner-pub-5631233433203577:vypgar-6zdh" />
+  <input type="hidden" name="ie" value="UTF-8" />
+  <label for="s" class="assistive-text"><?php _e( 'Search', 'twentyeleven' ); ?></label>
+  <input type="text" name="q" id="s" size="31" />
+  <input type="submit" name="sa" value="Search" class="submit" id="searchsubmit" />
 </form>
 
 <script type="text/javascript" src="http://www.google.co.uk/coop/cse/brand?form=cse-search-box&amp;lang=en"></script>
