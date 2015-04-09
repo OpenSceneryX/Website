724a79a312de615c1509c0216c085cb494bffbb7
Austin Goudge <austin@opensceneryx.com>
2015-04-09 20:55:34 +0100

    Change approach to handling library docs - now create a fake post (page) to send to Wordpress.

--- wp-content/plugins/opensceneryx/includes/class-opensceneryx.php
+++ wp-content/plugins/opensceneryx/includes/class-opensceneryx.php
@@ -29,7 +29,64 @@ class OpenSceneryX {
         $this->pluginDirPath = $pluginDirPath;
 
         // We want to intercept all requests to parse the URL
-        add_action('template_redirect', array($this, 'osxURLHandler'));
+        #add_action('template_redirect', array($this, 'osxURLHandler'));
+        add_action('wp', array($this, 'osxLibraryPage'));
+    }
+
+    function osxLibraryPage() {
+        global $wp_query;
+        
+        if (!empty($_SERVER['REQUEST_URI'])) {
+            $urlVars = explode('/', $_SERVER['REQUEST_URI']);
+
+            if (count($urlVars) < 2) {
+                return;
+            }
+
+            if (!$wp_query->is_404) {
+                return;
+            }
+
+		    switch ($urlVars[1]) {
+                case 'doc':
+                case 'facades':
+                case 'forests':
+                case 'lines':
+                case 'objects':
+                case 'polygons':
+                    break;
+                default:
+                    return;
+            }
+
+            $docPath = implode(array_slice($urlVars, 1, -1), '/');
+            $osxItemPath = ABSPATH . $docPath;
+
+            $id=-42;
+            $post = new stdClass();
+            $post->ID = $id;
+            $post->post_category = array('uncategorized'); //Add some categories. an array()???
+            $post->post_content = file_get_contents($osxItemPath);
+            $post->post_excerpt = '';
+            $post->post_status = 'publish';
+            $post->post_title = 'Fake Title';
+            $post->post_type = 'page';
+            $post->post_author = 1;
+            $post->post_parent = 111;
+            $post->guid = $osxItemPath;
+            
+            $wp_query->queried_object = $post;
+            $wp_query->post = $post;
+            $wp_query->found_posts = 1;
+            $wp_query->post_count = 1;
+            $wp_query->max_num_pages = 1;
+            $wp_query->is_single = 1;
+            $wp_query->is_404 = false;
+            $wp_query->is_posts_page = 1;
+            $wp_query->posts = array($post);
+            $wp_query->page = false;
+            $wp_query->is_post = false;
+        }
     }
 
     /**
