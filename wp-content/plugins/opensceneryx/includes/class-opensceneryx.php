9811a930ba8b2ecf455c8dac1b508e339eac356f
aussig <austin@opensceneryx.com>
2015-05-26 12:25:09 +0100

    Merged functionality from old plugin into new and deleted old plugin. Fixed highlighting of new links.

--- wp-content/plugins/opensceneryx/includes/class-opensceneryx.php
+++ wp-content/plugins/opensceneryx/includes/class-opensceneryx.php
@@ -35,12 +35,12 @@ class OpenSceneryX {
 
         add_shortcode('osxinfo', array($this, 'osxInfoShortcode'));
         add_shortcode('osxreleasenotes', array($this, 'osxReleaseNotesShortcode'));
+        add_shortcode('osxlinks', array($this, 'osxLinksShortcode'));
 
         add_filter('page_css_class', array($this, 'osxMenuClasses'), 10, 5);
         add_filter('pre_post_link', array($this, 'osxPermalink'));
         add_filter('the_content', array($this, 'osxContent'));
         add_filter('wpseo_breadcrumb_links', array($this, 'osxBreadcrumbs'));
-
     }
 
     function osxLibraryPage() {
@@ -137,6 +137,37 @@ class OpenSceneryX {
         } else {
             return "ERROR: No release notes found";
         }
+    }
+
+    function osxLinksShortcode($atts, $content = null) {
+        // Extract values from $attrs with defaults.  The values will be extracted directly into variables
+        // in the current symbol table
+        extract(shortcode_atts(array(
+                'linkcatid' => '0',
+                'cssclass' => 'multiple-airports',
+                ), $atts));
+
+        $bookmarks = get_bookmarks(array('category' => $linkcatid, 'show_description' => true, 'show_updated' => true));
+
+        $result = '<h2>' . $content . ' (' . count($bookmarks) . (count($bookmarks) == 1 ? ' site' : ' sites') . ')</h2>' . "\n";
+        $result .= '<ul class="xoxo blogroll">' . "\n";
+
+        $newLinkAge = 90 * 24 * 60 * 60;
+
+        foreach ($bookmarks as $bookmark) {
+            $name = esc_attr(sanitize_bookmark_field('link_name', $bookmark->link_name, $bookmark->link_id, 'display'));
+            $desc = esc_attr(sanitize_bookmark_field('link_description', $bookmark->link_description, $bookmark->link_id, 'display'));
+
+            $result .= '<li class="' . $cssclass . '"><a href="' . $bookmark->link_url . '"'
+                    . ($bookmark->link_target != '' ? ' target="' . $bookmark->link_target . '"' : '') . '>' . $name . '</a>'
+                    . ($desc ? ' - ' . $desc : '')
+                    . (time() - $bookmark->link_updated_f < $newLinkAge ? ' - <span>' . sprintf(__('NEW! %s'), date(get_option('links_updated_date_format'), $bookmark->link_updated_f + (get_option('gmt_offset') * 3600))) . '</span>' : '')
+                    . '</li>' . "\n";
+        }
+
+        $result .= '</ul>' . "\n";
+
+        return $result;
     }
 
     function osxMenuClasses($classes, $page, $depth, $args, $currentPage)
