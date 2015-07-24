e482cc2da2b25dad49ce66f68d26864831214216
aussig <austin@opensceneryx.com>
2015-07-24 16:12:14 +0100

    Upgraded to WP 4.2.3

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,14 +4,14 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2.2';
+$wp_version = '4.2.3';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
  *
  * @global int $wp_db_version
  */
-$wp_db_version = 31535;
+$wp_db_version = 31536;
 
 /**
  * Holds the TinyMCE version
@@ -33,3 +33,5 @@ $required_php_version = '5.2.4';
  * @global string $required_mysql_version
  */
 $required_mysql_version = '5.0';
+
+$wp_local_package = 'en_GB';
