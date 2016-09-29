7375db420d45bbd718fb9e67c30b4f280bf65332
aussig <austin@opensceneryx.com>
2016-09-29 15:56:09 +0100

    Modified URLs to https

--- custom/themes/opensceneryx/header.php
+++ custom/themes/opensceneryx/header.php
@@ -25,7 +25,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <title><?php wp_title(); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="profile" href="https://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
