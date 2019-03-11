e8451dd648daf0644853019667f1df7126cb5c8f
aussig <austin@opensceneryx.com>
2019-03-11 16:00:11 +0000

    Add object-specific documentation improvements as part of #9.

--- custom/plugins/opensceneryx/includes/class-osxobject.php
+++ custom/plugins/opensceneryx/includes/class-osxobject.php
@@ -11,6 +11,16 @@ class OSXObject extends OSXLibraryItem {
 
     protected $animated = false;
 
+    protected $lods = array();
+
+    protected $lightsCustom = false;
+    protected $lightsNamed = false;
+    protected $lightsParameterised = false;
+    protected $lightsSpill = false;
+
+    protected $tilted = false;
+    protected $smokeBlack = false;
+    protected $smokeWhite = false;
 
     function __construct($path, $url) {
         parent::__construct($path, $url);
@@ -41,6 +51,46 @@ class OSXObject extends OSXLibraryItem {
                 $this->animated = ($matches[1] == "True" || $matches[1] == "Yes");
                 continue;
             }
+
+            if (preg_match('/^LOD:\s+(.*?)\s+(.*)/', $line, $matches) === 1) {
+                $this->lods[] = array('min' => $matches[1], 'max' => $matches[2]);
+                continue;
+            }
+
+            if (preg_match('/^Custom Lights:\s+(.*)/', $line, $matches) === 1) {
+                $this->lightsCustom = ($matches[1] == "True" || $matches[1] == "Yes");
+                continue;
+            }
+
+            if (preg_match('/^Named Lights:\s+(.*)/', $line, $matches) === 1) {
+                $this->lightsNamed = ($matches[1] == "True" || $matches[1] == "Yes");
+                continue;
+            }
+
+            if (preg_match('/^Parameterised Lights:\s+(.*)/', $line, $matches) === 1) {
+                $this->lightsParameterised = ($matches[1] == "True" || $matches[1] == "Yes");
+                continue;
+            }
+
+            if (preg_match('/^Spill Lights:\s+(.*)/', $line, $matches) === 1) {
+                $this->lightsSpill = ($matches[1] == "True" || $matches[1] == "Yes");
+                continue;
+            }
+
+            if (preg_match('/^Tilted:\s+(.*)/', $line, $matches) === 1) {
+                $this->tilted = ($matches[1] == "True" || $matches[1] == "Yes");
+                continue;
+            }
+
+            if (preg_match('/^Black Smoke:\s+(.*)/', $line, $matches) === 1) {
+                $this->smokeBlack = ($matches[1] == "True" || $matches[1] == "Yes");
+                continue;
+            }
+
+            if (preg_match('/^White Smoke:\s+(.*)/', $line, $matches) === 1) {
+                $this->smokeWhite = ($matches[1] == "True" || $matches[1] == "Yes");
+                continue;
+            }
         }
     }
 
@@ -59,6 +109,40 @@ class OSXObject extends OSXLibraryItem {
 
         if ($this->animated) {
             $result .= "<li class='animated'><span class='fieldValue'>Animated</span></li>\n";
+        }
+
+        if (count($this->lods) > 0) {
+            foreach ($this->lods as $lod) {
+                $result .= "<li><span class='fieldTitle'>LOD Range:</span> <span class='fieldValue'>" . $lod['min'] . " to " . $lod['max'] . "</span></li>\n";
+            }
+        }
+
+        if ($this->lightsCustom) {
+            $result .= "<li><span class='fieldValue'>Contains Custom Lights</span></li>\n";
+        }
+
+        if ($this->lightsNamed) {
+            $result .= "<li><span class='fieldValue'>Contains Named Lights</span></li>\n";
+        }
+
+        if ($this->lightsParameterised) {
+            $result .= "<li><span class='fieldValue'>Contains Parameterised Lights</span></li>\n";
+        }
+
+        if ($this->lightsSpill) {
+            $result .= "<li><span class='fieldValue'>Contains Spill Lights</span></li>\n";
+        }
+
+        if ($this->tilted) {
+            $result .= "<li><span class='fieldValue'>Will Render Tilted on Sloped Terrain</span></li>\n";
+        }
+
+        if ($this->smokeBlack) {
+            $result .= "<li><span class='fieldValue'>Emits Black Smoke</span></li>\n";
+        }
+
+        if ($this->smokeWhite) {
+            $result .= "<li><span class='fieldValue'>Emits White Smoke</span></li>\n";
         }
 
         if ($result != "") {
