56b8693640b3f64060f358eb45fea796f039a280
aussig <austin@opensceneryx.com>
2019-04-01 12:31:14 +0100

    Add tooltip help to object documentation

--- custom/plugins/opensceneryx/includes/class-osxobject.php
+++ custom/plugins/opensceneryx/includes/class-osxobject.php
@@ -106,7 +106,7 @@ class OSXObject extends OSXLibraryItem {
         $result = "";
 
         if ($this->width !== null && $this->height !== null && $this->depth !== null) {
-            $result .= "<li><span class='fieldTitle'>Dimensions:</span>\n";
+            $result .= "<li><span class='fieldTitle'>Dimensions</span> <dfn class='tooltip'>ⓘ<span>These are the outermost bounds of the object.</span></dfn>:\n";
             $result .= "<ul class='dimensions'>\n";
             $result .= "<li id='width'><span class='fieldTitle'>w:</span> " . $this->width . "m</li>\n";
             $result .= "<li id='height'><span class='fieldTitle'>h:</span> " . $this->height . "m</li>\n";
@@ -116,7 +116,7 @@ class OSXObject extends OSXLibraryItem {
         }
 
         if ($this->animated) {
-            $result .= "<li class='animated'><span class='fieldValue'>Animated</span></li>\n";
+            $result .= "<li class='animated'><span class='fieldValue'>Animated</span> <dfn class='tooltip'>ⓘ<span>This object contains animation.</span></dfn></li>\n";
         }
 
         if (count($this->lods) > 0) {
@@ -126,35 +126,35 @@ class OSXObject extends OSXLibraryItem {
         }
 
         if ($this->lightsCustom) {
-            $result .= "<li><span class='fieldValue'>Contains Custom Lights</span></li>\n";
+            $result .= "<li><span class='fieldValue'>Contains Custom Lights</span> <dfn class='tooltip'>ⓘ<span>This object contains custom lighting effects. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#LIGHT_CUSTOM_ltxgt_ltygt_ltzgt_ltrgt_ltggt_ltbgt_ltagt_ltsgt_lts1gt_ltt1gt_lts2gt_ltt2gt_ltdatarefgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
         }
 
         if ($this->lightsNamed) {
-            $result .= "<li><span class='fieldValue'>Contains Named Lights</span></li>\n";
+            $result .= "<li><span class='fieldValue'>Contains Named Lights</span> <dfn class='tooltip'>ⓘ<span>This object contains named lighting effects. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#LIGHT_NAMED_ltnamegt_ltxgt_ltygt_ltzgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
         }
 
         if ($this->lightsParameterised) {
-            $result .= "<li><span class='fieldValue'>Contains Parameterised Lights</span></li>\n";
+            $result .= "<li><span class='fieldValue'>Contains Parameterised Lights</span> <dfn class='tooltip'>ⓘ<span>This object contains parameterised lighting effects. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#LIGHT_PARAM_ltnamegt_ltxgt_ltygt_ltzgt_ltadditional_paramsgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
         }
 
         if ($this->lightsSpill) {
-            $result .= "<li><span class='fieldValue'>Contains Spill Lights</span></li>\n";
+            $result .= "<li><span class='fieldValue'>Contains Spill Lights</span> <dfn class='tooltip'>ⓘ<span>This object contains spill lighting effects. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#LIGHT_SPILL_CUSTOM_ltxgt_ltygt_ltzgt_ltrgt_ltggt_ltbgt_ltagt_ltsgt_ltdxgt_ltdygt_ltdzgt_ltsemigt_ltdrefgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
         }
 
         if ($this->tilted) {
-            $result .= "<li><span class='fieldValue'>Will Render Tilted on Sloped Terrain</span></li>\n";
+            $result .= "<li><span class='fieldValue'>Will Render Tilted</span> <dfn class='tooltip'>ⓘ<span>The base of this object will be aligned with the underlying terrain, which means it may be tilted if the terrain is sloping. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#TILTED' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
         }
 
         if ($this->smokeBlack) {
-            $result .= "<li><span class='fieldValue'>Emits Black Smoke</span></li>\n";
+            $result .= "<li><span class='fieldValue'>Emits Black Smoke</span> <dfn class='tooltip'>ⓘ<span>This object emits black smoke. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#smoke_black_ltxgt_ltygt_ltzgt_ltsgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
         }
 
         if ($this->smokeWhite) {
-            $result .= "<li><span class='fieldValue'>Emits White Smoke</span></li>\n";
+            $result .= "<li><span class='fieldValue'>Emits White Smoke</span> <dfn class='tooltip'>ⓘ<span>This object emits white smoke. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#smoke_white_ltxgt_ltygt_ltzgt_ltsgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
         }
 
         if ($this->wedRotationLockAngle != null) {
-            $result .= "<li><span class='fieldTitle'>Placement Locked at:</span> <span class='fieldValue'>" . $this->wedRotationLockAngle . "°</span> <span class='fieldTitle'>in WED</span></li>\n";
+            $result .= "<li><span class='fieldTitle'>Placement Locked at: </span> <span class='fieldValue'>" . $this->wedRotationLockAngle . "°</span> <span class='fieldTitle'>in WED</span> <dfn class='tooltip'>ⓘ<span>Scenery developers, when adding this object using <a href='https://developer.x-plane.com/tools/worldeditor/' target='_blank'>WED version 2.1 or higher</a>, the rotation will be locked at " . $this->wedRotationLockAngle . "°. This is because this object is designed to rotate in the wind, and to align with the correct wind direction in X-Plane® the placement angle must be locked at this value. If you are using <a href='https://marginal.org.uk/x-planescenery/tools.html' target='_blank'>OverlayEditor</a> or an older version of <a href='https://developer.x-plane.com/tools/worldeditor/' target='_blank'>WED</a> then please ensure you set the rotation to " . $this->wedRotationLockAngle . "°.</span></dfn></li>\n";
         }
 
         if ($result != "") {
