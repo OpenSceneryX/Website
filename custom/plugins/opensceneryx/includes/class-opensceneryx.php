c6aba97c45c522d85dc95a26e9e137d65bf8399f
aussig <austin@opensceneryx.com>
2019-10-12 18:18:18 +0100

    Reinstated slick slider (required on home page and release notes page)

--- custom/plugins/opensceneryx/includes/class-opensceneryx.php
+++ custom/plugins/opensceneryx/includes/class-opensceneryx.php
@@ -127,6 +127,7 @@ class OpenSceneryX {
         // Required by slick carousel
         wp_enqueue_script('jQuery', '//code.jquery.com/jquery-1.11.0.min.js', array(), false, true);
         wp_enqueue_script('jQuery-migrate', '//code.jquery.com/jquery-migrate-1.2.1.min.js', array(), false, true);
+        wp_enqueue_script('slick', plugin_dir_url(__FILE__) . 'slick/slick.min.js', array(), false, true);
         // Required by three.js 3d renderer
         wp_enqueue_script('three.js', '//cdnjs.cloudflare.com/ajax/libs/three.js/107/three.min.js', array(), false, true);
         wp_enqueue_script('3ddsloader', plugin_dir_url(__FILE__) . 'three.js/DDSLoader.js', array('three.js'), false, true);
