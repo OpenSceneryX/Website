b518749744471aec37f5fbf3e101b99bddd6ba16
aussig <austin@opensceneryx.com>
2019-12-12 18:15:46 +0000

    Add support for decals.

---
+++ custom/plugins/opensceneryx/includes/class-osxdecal.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Description of OSXDecal
+ */
+class OSXDecal extends OSXLibraryItem {
+
+
+    function __construct($path, $url) {
+        parent::__construct($path, $url);
+    }
+
+    function enqueueScript() {
+        parent::enqueueScript();
+
+        // Inject the slick slider code. Can't do this in the getHTML function below because that's too late
+        $slickScript = '<script type="text/javascript">
+            $(document).ready(function(){
+                $(".slick-screenshots").slick({
+                    autoplay: true,
+                    autoplaySpeed: 2000,
+                    swipeToSlide: true,
+                    dots: true
+                });
+            });
+            </script>';
+
+        wp_enqueue_script('slick', plugin_dir_url(__FILE__) . 'slick/slick.min.js', array(), false, true);
+        wp_add_inline_script('slick', $slickScript, 'after');
+    }
+
+    protected function parse() {
+        parent::parse();
+    }
+
+    protected function getTypeSpecificHTML() {
+        $result = "";
+
+        return $result;
+    }
+
+    protected function getTypeExtension() {
+        return ".dcl";
+    }
+}
