b80ab968dba50d6c4b8d3e0510fbe95fa0f2342c
aussig <austin@opensceneryx.com>
2020-04-04 10:12:08 +0100

    Make field titles vs field values more consistent on item pages - if a property of an item is a "boolean" (i.e. present or not), show it as a field title.

--- custom/plugins/opensceneryx/includes/class-osxobject.php
+++ custom/plugins/opensceneryx/includes/class-osxobject.php
@@ -207,7 +207,7 @@ class OSXObject extends OSXLibraryItem {
         }
 
         if ($this->animated) {
-            $result .= "<li class='animated'><span class='fieldValue'>Animated</span> <dfn class='tooltip'>ⓘ<span>This object contains animation.</span></dfn></li>\n";
+            $result .= "<li class='animated'><span class='fieldTitle'>Animated</span> <dfn class='tooltip'>ⓘ<span>This object contains animation.</span></dfn></li>\n";
         }
 
         if (count($this->lods) > 0) {
@@ -217,31 +217,31 @@ class OSXObject extends OSXLibraryItem {
         }
 
         if ($this->lightsCustom) {
-            $result .= "<li><span class='fieldValue'>Contains Custom Lights</span> <dfn class='tooltip'>ⓘ<span>This object contains custom lighting effects. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#LIGHT_CUSTOM_ltxgt_ltygt_ltzgt_ltrgt_ltggt_ltbgt_ltagt_ltsgt_lts1gt_ltt1gt_lts2gt_ltt2gt_ltdatarefgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
+            $result .= "<li><span class='fieldTitle'>Contains Custom Lights</span> <dfn class='tooltip'>ⓘ<span>This object contains custom lighting effects. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#LIGHT_CUSTOM_ltxgt_ltygt_ltzgt_ltrgt_ltggt_ltbgt_ltagt_ltsgt_lts1gt_ltt1gt_lts2gt_ltt2gt_ltdatarefgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
         }
 
         if ($this->lightsNamed) {
-            $result .= "<li><span class='fieldValue'>Contains Named Lights</span> <dfn class='tooltip'>ⓘ<span>This object contains named lighting effects. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#LIGHT_NAMED_ltnamegt_ltxgt_ltygt_ltzgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
+            $result .= "<li><span class='fieldTitle'>Contains Named Lights</span> <dfn class='tooltip'>ⓘ<span>This object contains named lighting effects. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#LIGHT_NAMED_ltnamegt_ltxgt_ltygt_ltzgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
         }
 
         if ($this->lightsParameterised) {
-            $result .= "<li><span class='fieldValue'>Contains Parameterised Lights</span> <dfn class='tooltip'>ⓘ<span>This object contains parameterised lighting effects. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#LIGHT_PARAM_ltnamegt_ltxgt_ltygt_ltzgt_ltadditional_paramsgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
+            $result .= "<li><span class='fieldTitle'>Contains Parameterised Lights</span> <dfn class='tooltip'>ⓘ<span>This object contains parameterised lighting effects. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#LIGHT_PARAM_ltnamegt_ltxgt_ltygt_ltzgt_ltadditional_paramsgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
         }
 
         if ($this->lightsSpill) {
-            $result .= "<li><span class='fieldValue'>Contains Spill Lights</span> <dfn class='tooltip'>ⓘ<span>This object contains spill lighting effects. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#LIGHT_SPILL_CUSTOM_ltxgt_ltygt_ltzgt_ltrgt_ltggt_ltbgt_ltagt_ltsgt_ltdxgt_ltdygt_ltdzgt_ltsemigt_ltdrefgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
+            $result .= "<li><span class='fieldTitle'>Contains Spill Lights</span> <dfn class='tooltip'>ⓘ<span>This object contains spill lighting effects. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#LIGHT_SPILL_CUSTOM_ltxgt_ltygt_ltzgt_ltrgt_ltggt_ltbgt_ltagt_ltsgt_ltdxgt_ltdygt_ltdzgt_ltsemigt_ltdrefgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
         }
 
         if ($this->tilted) {
-            $result .= "<li><span class='fieldValue'>Will Render Tilted</span> <dfn class='tooltip'>ⓘ<span>The base of this object will be aligned with the underlying terrain, which means it may be tilted if the terrain is sloping. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#TILTED' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
+            $result .= "<li><span class='fieldTitle'>Will Render Tilted</span> <dfn class='tooltip'>ⓘ<span>The base of this object will be aligned with the underlying terrain, which means it may be tilted if the terrain is sloping. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#TILTED' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
         }
 
         if ($this->smokeBlack) {
-            $result .= "<li><span class='fieldValue'>Emits Black Smoke</span> <dfn class='tooltip'>ⓘ<span>This object emits black smoke. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#smoke_black_ltxgt_ltygt_ltzgt_ltsgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
+            $result .= "<li><span class='fieldTitle'>Emits Black Smoke</span> <dfn class='tooltip'>ⓘ<span>This object emits black smoke. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#smoke_black_ltxgt_ltygt_ltzgt_ltsgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
         }
 
         if ($this->smokeWhite) {
-            $result .= "<li><span class='fieldValue'>Emits White Smoke</span> <dfn class='tooltip'>ⓘ<span>This object emits white smoke. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#smoke_white_ltxgt_ltygt_ltzgt_ltsgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
+            $result .= "<li><span class='fieldTitle'>Emits White Smoke</span> <dfn class='tooltip'>ⓘ<span>This object emits white smoke. For more information, <a href='https://developer.x-plane.com/article/obj8-file-format-specification/#smoke_white_ltxgt_ltygt_ltzgt_ltsgt' target='_blank'>see the official documentation</a>.</span></dfn></li>\n";
         }
 
         if ($this->wedRotationLockAngle != null) {
